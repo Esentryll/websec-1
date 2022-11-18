8e931f0c114db39a00c97455eb4e528c5ce32f3a
Esentryll <you@example.com>
2022-11-18 23:15:09 +0400

    fixed field checking and div by zero

--- script.js
+++ script.js
@@ -8,26 +8,38 @@ function buttonClickHandler()
     let value1 = document.getElementById("input1").value;
     let value2 = document.getElementById("input2").value;
 
-    var result = 0;
-    switch (selectorElem.value){
-        case 'plus':
-            result = parseFloat(value1) + parseFloat(value2);
-            break;
-        case 'minus':
-            if (value1>value2){
-                result = parseFloat(value1) - parseFloat(value2);
-                break;
-            }
-        case 'mult':
-            result = parseFloat(value1) * parseFloat(value2);
-            break;
-        case 'div':
-            if (value2!=0){
-                result = parseFloat(value1) / parseFloat(value2);
-                break;
-            }
+    if (value1==="" || value2===""){
+        alert("Введите значения!");
     }
-    document.getElementById("results").textContent = result;
-    pastResult = result;
-    console.log(value1,value2)
+    else{
+
+        var result = 0;
+
+        switch (selectorElem.value){
+            case 'plus':
+                result = parseFloat(value1) + parseFloat(value2);
+                break;
+            case 'minus':
+                if (value1>value2){
+                    result = parseFloat(value1) - parseFloat(value2);
+                    break;
+                }
+            case 'mult':
+                result = parseFloat(value1) * parseFloat(value2);
+                break;
+            case 'div':
+                if (Math.abs(value2) < Number.EPSILON){
+                    alert("Делить на ноль нельзя!")
+                    break;
+                }
+                else{
+                    result = parseFloat(value1) / parseFloat(value2);
+                    break;
+                }
+        }
+
+        document.getElementById("results").textContent = result;
+        console.log(value1,value2)
+    }
+
 }
